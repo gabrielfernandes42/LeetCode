ae399f1605e464a0cff8848986ea9087b29ee410
gabriel <gabrielfernandes254@gmail.com>
2023-02-07 21:29:03 -0300

    Runtime : 9 ms - Memory 44.5 MB

---
+++ two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+
+const twoSum = function (nums, target) {
+  let memory = {};
+  for (let [index, num] of nums.entries()) {
+    if (memory[num] !== undefined) return [memory[num], index];
+    memory[target - num] = index;
+  }
+};
